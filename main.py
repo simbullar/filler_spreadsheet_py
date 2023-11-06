8bb4eabb6c77fd8f670483e3d7294f76913f922c
Roman <86300574+simbullar@users.noreply.github.com>
2023-11-06 07:20:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import gspread
 
-spreadsheet_id = "YOUR_SPREADSHEET_ID"
+spreadsheet_id = "SPREADSHEET ID HERE"
 
 creds = service_account.Credentials.from_service_account_file("key.json", scopes=["https://www.googleapis.com/auth/spreadsheets", 'https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive'])
 
 client = gspread.authorize(creds)
 
-sheet = client.open('YOUR_SPREADSHEET_NAME')
+sheet = client.open('SPREADSHEET NAME HERE')
 
 sheet_instance = sheet.get_worksheet(0)
 
@@ -21,8 +21,8 @@ f = open("something.txt", "r")
 i = 1
 for line in f:
     i = i + 1
-    last = get_last_n_characters(line, 2)
-    need_to_write = "sw1-" + last
-    print(need_to_write)
-    cell = sheet_instance.cell(col=4, row=i)
-    cell.fill(line)
+    try:
+        last = int(get_last_n_characters(line, 2))
+        sheet_instance.update(range_name='D' + str(i), values="sw1-" + str(last))
+    except ValueError:
+        print("String")
