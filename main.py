a0155ddc955f1d9a1aae557a5a91cb9a19ad8906
Roman <86300574+simbullar@users.noreply.github.com>
2023-11-10 11:21:43 +0200

    commit 3
    added detection of cells, also name of spreadsheet is not needed anymore

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import gspread
 
-spreadsheet_id = "SPREADSHEET ID HERE"
+spreadsheet_id = "YOUR_SPREADSHEET_ID"
 
 creds = service_account.Credentials.from_service_account_file("key.json", scopes=["https://www.googleapis.com/auth/spreadsheets", 'https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive'])
 
 client = gspread.authorize(creds)
 
-sheet = client.open('SPREADSHEET NAME HERE')
+sheet = client.open_by_key(spreadsheet_id)
 
 sheet_instance = sheet.get_worksheet(0)
 
@@ -21,8 +21,8 @@ f = open("something.txt", "r")
 i = 1
 for line in f:
     i = i + 1
-    try:
-        last = int(get_last_n_characters(line, 2))
-        sheet_instance.update(range_name='D' + str(i), values="sw1-" + str(last))
-    except ValueError:
-        print("String")
+    last = get_last_n_characters(line, 2)
+    cell = sheet_instance.findall((line[:-2]).replace(" ", ""))
+    print((line[:-2]).replace(" ", ""))
+    print(cell)
+#!    sheet_instance.update(range_name='D' + str(cell), values="sw1-" + str(last))
